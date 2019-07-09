c0912fb6e65a963666588fc50b82eedcf92c7ddf
amir aleosfoor <amirarcs@gmail.com>
2019-07-09 17:41:51 +0430

    xml update

---
+++ app/src/main/java/ir/bolive/app/jamisapp/ActivityMainPage.java
@@ -0,0 +1,14 @@
+package ir.bolive.app.jamisapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
