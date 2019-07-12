bff9dedd7d60a426c84d8d1825856b984795fd64
Hamid <amirarcs@gmail.com>
2019-07-12 17:36:07 +0430

    Project Updated

--- app/src/main/java/ir/bolive/app/jamisapp/database/DatabaseHelper.java
+++ app/src/main/java/ir/bolive/app/jamisapp/database/DatabaseHelper.java
@@ -1,8 +1,16 @@
 package ir.bolive.app.jamisapp.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ir.bolive.app.jamisapp.models.Patient;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -26,7 +34,93 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     //PATIENT CRUD
+    public List<Patient> getAllPatients(){
+        SQLiteDatabase db=this.getWritableDatabase();
+        List<Patient> patientList=new ArrayList<Patient>();
+        String query = "SELECT * FROM " + DB_Constants.TABLE_PATIENT + " ORDER BY " + DB_Constants.FULLNAME;
+        Cursor cursor=db.rawQuery(query,null);
+        if(cursor.moveToFirst()){
+            do{
+                Patient patient=new Patient();
+                patient.setPid(cursor.getInt(cursor.getColumnIndex(DB_Constants.PATIENT_ID)));
+                patient.setFullname(cursor.getString(cursor.getColumnIndex(DB_Constants.FULLNAME)));
+                patient.setNationalcode(cursor.getString(cursor.getColumnIndex(DB_Constants.NATIONAL_CODE)));
+                patient.setPhone(cursor.getString(cursor.getColumnIndex(DB_Constants.PHONE)));
+                patient.setRefdate(cursor.getString(cursor.getColumnIndex(DB_Constants.REF_DATE)));
+                patientList.add(patient);
+            }while(cursor.moveToNext());
+        }
+        cursor.close();
+        return patientList;
+    }
+    public Patient getPatient(String nationalCode,String fullname){
+        SQLiteDatabase db=this.getReadableDatabase();
+        Patient patient=new Patient();
+        Cursor cursor=null;
+        if(nationalCode!=null){
+             cursor=db.query(DB_Constants.TABLE_PATIENT,new String[]{DB_Constants.PATIENT_ID,DB_Constants.FULLNAME,
+                            DB_Constants.NATIONAL_CODE,DB_Constants.PHONE,DB_Constants.REF_DATE},DB_Constants.NATIONAL_CODE+"=?",
+                    new String[]{nationalCode},null,null,null);
+        }
+        else if(fullname!=null){
+             cursor=db.query(DB_Constants.TABLE_PATIENT,new String[]{DB_Constants.PATIENT_ID,DB_Constants.FULLNAME,
+                            DB_Constants.NATIONAL_CODE,DB_Constants.PHONE,DB_Constants.REF_DATE},DB_Constants.NATIONAL_CODE+"=?",
+                    new String[]{fullname},null,null,null);
+        }
 
+        if (cursor!=null){
+            cursor.moveToFirst();
+            patient.setPid(cursor.getInt(cursor.getColumnIndex(DB_Constants.PATIENT_ID)));
+            patient.setFullname(cursor.getString(cursor.getColumnIndex(DB_Constants.FULLNAME)));
+            patient.setNationalcode(cursor.getString(cursor.getColumnIndex(DB_Constants.NATIONAL_CODE)));
+            patient.setPhone(cursor.getString(cursor.getColumnIndex(DB_Constants.PHONE)));
+            patient.setRefdate(cursor.getString(cursor.getColumnIndex(DB_Constants.REF_DATE)));
+            cursor.close();
+        }
+        return patient;
+    }
+    public boolean insetPatient(Patient patient){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(DB_Constants.FULLNAME, patient.getFullname());
+        values.put(DB_Constants.PHONE, patient.getPhone());
+        values.put(DB_Constants.NATIONAL_CODE, patient.getNationalcode());
+        values.put(DB_Constants.REF_DATE, patient.getRefdate());
+        long rowId=db.insert(DB_Constants.TABLE_PATIENT,null,values);
+        if (rowId>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean updatePatient(Patient patient){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(DB_Constants.FULLNAME, patient.getFullname());
+        values.put(DB_Constants.PHONE, patient.getPhone());
+        values.put(DB_Constants.NATIONAL_CODE, patient.getNationalcode());
+        values.put(DB_Constants.REF_DATE, patient.getRefdate());
+        int affected=db.update(DB_Constants.TABLE_PATIENT,values,DB_Constants.PATIENT_ID+"=?",
+                new String[]{String.valueOf(patient.getPid())});
+        if (affected>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean deletePatient(int pid){
+        SQLiteDatabase db=this.getWritableDatabase();
+        int affected =db.delete(DB_Constants.TABLE_PATIENT,DB_Constants.PATIENT_ID+"=?",
+                new String[]{String.valueOf(pid)});
+        if (affected>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     //GALLERY CRUD
 
     //FACE CRUD
